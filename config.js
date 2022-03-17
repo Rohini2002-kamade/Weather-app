21ed1c5275a45f41a74c8ebf30e8263eb0191651
Rohini Kamade <rohinikamade@gmail.com>
2022-03-17 15:50:57 +0530

    added my project

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "9141df2d2828c48fdfc6d14347a33a8d"
+    }
+}
+
+module.exports = constants;
